a9304e340b836dc06bd9c2c2617a1a16c339800e
Matheus Costa <mathws.costag@gmail.com>
2021-11-04 02:29:49 -0300

    Configura a store de user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import Routes from './Routes';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter store={ store }>
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ src/actions/user.js
@@ -0,0 +1,3 @@
+import user from '../slices/user';
+
+export default { changeUser, changeUserEmail, changeUserName, logout } = user.actions;
